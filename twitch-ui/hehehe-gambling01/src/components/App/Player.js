4841bb53008b6e3148856f6cdde1e54fac2a176b
Stanley Yu <geniustanley@gmail.com>
2019-11-17 09:43:38 +0800

    Add additional information and viewer empty state

--- twitch-ui/hehehe-gambling01/src/components/App/Player.js
+++ twitch-ui/hehehe-gambling01/src/components/App/Player.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Button, ListGroup, Badge, Row, Col } from 'react-bootstrap';
-import moment from 'moment';
+import React, { Component } from "react";
+import { Button, ListGroup, Badge, Row, Col } from "react-bootstrap";
+import moment from "moment";
 
 export default class Player extends Component {
   constructor(props) {
@@ -9,54 +9,63 @@ export default class Player extends Component {
     this.renderCountdown = this.renderCountdown.bind(this);
     this.exchangeBits = this.exchangeBits.bind(this);
     this.state = {
-      title: '對面李星會死幾次呢？',
-      countdown: '00 : 00',
+      start: false,
+      title: "對面李星會死幾次呢？",
+      countdown: "00 : 00",
       winner: null,
       options: [
         {
-          option: '5次以下',
+          option: "5次以下",
           total: 30,
+          people: {}
         },
         {
-          option: '5~10次',
+          option: "5~10次",
           total: 100,
+          people: {}
         },
         {
-          option: '11~15次',
+          option: "11~15次",
           total: 200,
-        },
+          people: {}
+        }
       ],
+      winners: {}
     };
     Twitch.ext.bits.onTransactionComplete(function(transaction) {
       const userId = props.authentication.getUserId();
       const amount = transaction.product.cost.amount;
-      console.log('onTransactionComplete', userId, amount);
+      console.log("onTransactionComplete", userId, amount);
       fetch(`http://127.0.0.1:3000/cash-in`, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({
           twitchWatcherId: userId,
-          amount,
+          amount
         }),
         headers: new Headers({
-          'Content-Type': 'application/json',
-        }),
+          "Content-Type": "application/json"
+        })
       });
     });
   }
 
   fetchGame() {
     fetch(`http://localhost:3000/game?streamerId=12345`, {
-      method: 'GET',
+      method: "GET"
     })
       .then(r => r.json())
       .then(a => {
-        this.setState({
-          title: a.title,
-          countdown: a.countdown,
-          options: a.options,
-          createdAt: a.createdAt,
-          winner: a.winner,
-        });
+        if (a) {
+          this.setState({
+            start: true,
+            title: a.title,
+            countdown: a.countdown,
+            options: a.options,
+            createdAt: a.createdAt,
+            winner: a.winner,
+            winners: a.winners || {}
+          });
+        }
       });
   }
 
@@ -71,11 +80,11 @@ export default class Player extends Component {
     const createdAt = moment(this.state.createdAt);
     const now = moment(new Date());
 
-    const diff = now.diff(createdAt, 'seconds');
+    const diff = now.diff(createdAt, "seconds");
 
     const countdown = this.state.countdown - diff;
     if (countdown >= 0) {
-      const formatted = moment.utc(countdown * 1000).format('mm : ss');
+      const formatted = moment.utc(countdown * 1000).format("mm : ss");
       return (
         <h3>
           <b>{formatted}</b>
@@ -95,90 +104,125 @@ export default class Player extends Component {
   }
 
   render() {
-    return (
-      <div style={{ margin: 30 }}>
-        <Row>
-          <Col>
-            <h2
-              className="text-center"
-              style={{ marginBottom: 0, fontWeight: 'bold', color: 'white' }}
-            >
-              {this.state.title}
-            </h2>
-          </Col>
-        </Row>
+    const winAmount =
+      this.state.winners[this.props.authentication.getUserId()] || 0;
+    if (this.state.start) {
+      return (
+        <div style={{ margin: 30 }}>
+          <Row>
+            <Col>
+              <h2
+                className="text-center"
+                style={{ marginBottom: 0, fontWeight: "bold", color: "white" }}
+              >
+                {this.state.title}
+              </h2>
+            </Col>
+          </Row>
 
-        <hr
-          style={{
-            border: 0,
-            borderTop: '1px solid rgba(255, 255, 255)',
-          }}
-        />
+          <hr
+            style={{
+              border: 0,
+              borderTop: "1px solid rgba(255, 255, 255)"
+            }}
+          />
 
-        <Row>
-          <Col>
-            <h5
-              className="text-center"
-              style={{ fontWeight: 'bold', color: 'white' }}
-            >
-              {this.state.winner == null ? (
-                this.renderCountdown()
-              ) : (
-                <h3>
-                  <b>答案為 {this.state.options[this.state.winner].option}</b>
-                </h3>
-              )}
-            </h5>
-          </Col>
-        </Row>
+          <Row>
+            <Col>
+              <h5
+                className="text-center"
+                style={{ fontWeight: "bold", color: "white" }}
+              >
+                {this.state.winner == null ? (
+                  this.renderCountdown()
+                ) : (
+                  <div>
+                    <h3>
+                      <b>
+                        答案為 {this.state.options[this.state.winner].option}
+                      </b>
+                    </h3>
+                    <h3>
+                      <b>您獲得了 {winAmount} 元</b>
+                    </h3>
+                  </div>
+                )}
+              </h5>
+            </Col>
+          </Row>
 
-        <Row>
-          <Col>
-            <ListGroup style={{ background: 'transparent' }}>
-              {this.state.options.map((o, i) => {
-                return (
-                  <ListGroup.Item
-                    style={{
-                      borderColor: 'white',
-                      color: 'white',
-                    }}
-                    className={
-                      i == this.state.winner ? 'winnerActive' : 'transparentBg'
-                    }
-                  >
-                    <div
+          <Row>
+            <Col>
+              <ListGroup style={{ background: "transparent" }}>
+                {this.state.options.map((o, i) => {
+                  return (
+                    <ListGroup.Item
                       style={{
-                        lineHeight: '36px',
+                        borderColor: "white",
+                        color: "white"
                       }}
+                      className={
+                        i == this.state.winner
+                          ? "winnerActive"
+                          : "transparentBg"
+                      }
                     >
-                      {o.option}
-                      <Button
-                        className={['fakebadge', 'float-right']}
-                        variant="outline-light"
-                        onClick={() => {}}
+                      <div
+                        style={{
+                          lineHeight: "36px"
+                        }}
                       >
-                        $ {o.total}
-                      </Button>
-                    </div>
-                  </ListGroup.Item>
-                );
-              })}
-            </ListGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button
-              className="submitform"
-              variant="outline-light"
-              style={{ marginTop: '1rem' }}
-              onClick={this.exchangeBits}
-            >
-              儲存小奇點
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    );
+                        {o.option}
+                        {`   -   `}
+                        {Object.keys(o.people).length} 人下注
+                        <Button
+                          className={["fakebadge", "float-right"]}
+                          variant="outline-light"
+                          onClick={() => {}}
+                        >
+                          $ {o.total}
+                        </Button>
+                      </div>
+                    </ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button
+                className="submitform"
+                variant="outline-light"
+                style={{ marginTop: "1rem" }}
+                onClick={this.exchangeBits}
+              >
+                儲存小奇點
+              </Button>
+            </Col>
+          </Row>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Row>
+            <Col>
+              <h1
+                style={{
+                  marginTop: 105,
+                  marginBottom: 100,
+                  color: "white",
+                  fontWeight: "bold"
+                }}
+                className="text-center"
+              >
+                遊戲尚未開始
+              </h1>
+            </Col>
+          </Row>
+        </div>
+      );
+    }
   }
 }
